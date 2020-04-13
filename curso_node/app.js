1ebd0eeb231c6a0781b2cd13725d80478a1b1f13
123PedroAugusto1 <45266843+123PedroAugusto1@users.noreply.github.com>
2020-04-13 20:06:46 -0300

    Foi adicionado os modulos do projeto e o EJS

---
+++ curso_node/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+    res.render("home/index");
+})
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia');
+})
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias');
+})
+app.listen(8181, function(){
+    console.log("Server está ok");
+})
