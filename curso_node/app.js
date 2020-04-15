836ed854eb04604f31acec6665b4e2fccbbb4a80
123PedroAugusto1 <45266843+123PedroAugusto1@users.noreply.github.com>
2020-04-14 21:01:25 -0300

    ImportaÃÃo de modulos para mysql

--- curso_node/app.js
+++ curso_node/app.js
@@ -1,13 +1,9 @@
 var app = require('./config/server');
-var rota_home = require('./app/routes/home');
-var rota_noticia = require('./app/routes/noticias');
-var rota_inclusao = require('./app/routes/formulario_inclusao_noticia');
 
-rota_home(app);
-rota_noticia(app);
-rota_inclusao(app);
-
-
+//Rotas do projeto
+var rota_home = require('./app/routes/home')(app);
+var rota_noticia = require('./app/routes/noticias')(app);
+var rota_inclusao = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(8181, function(){
     console.log("Servidor on");
