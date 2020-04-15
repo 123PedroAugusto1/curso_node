fcaa9fc2f866983fbd521bc6f47c32a72d8be9fd
123PedroAugusto1 <45266843+123PedroAugusto1@users.noreply.github.com>
2020-04-15 19:53:37 -0300

    refactoring the code

--- curso_node/app.js
+++ curso_node/app.js
@@ -1,9 +1,9 @@
 var app = require('./config/server');
 
 //Rotas do projeto
-var rota_home = require('./app/routes/home')(app);
-var rota_noticia = require('./app/routes/noticias')(app);
-var rota_inclusao = require('./app/routes/formulario_inclusao_noticia')(app);
+//var rota_home = require('./app/routes/home')(app);
+//var rota_noticia = require('./app/routes/noticias')(app);
+//var rota_inclusao = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(8181, function(){
     console.log("Servidor on");
--- curso_node/app/routes/noticias.js
+++ curso_node/app/routes/noticias.js
@@ -1,5 +1,12 @@
-module.exports =function(app){
-    app.get('/noticias', function(req, res){
-        res.render('noticias/noticias');
+module.exports = function(app){
+
+    app.get('/noticias', function(req,res){
+
+        var connection = app.config.dbConnection();
+
+        connection.query("SELECT * FROM noticias", function(error, result){
+            res.render('noticias/noticias', { noticias : result });
+        });
+
     });
 }
